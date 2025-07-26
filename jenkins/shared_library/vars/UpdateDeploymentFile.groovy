8d26b89dc4659a55cac85a974d84b693fe93da31
Ahmed Orabi <ahmedorabi11011@gmail.com>
2025-07-26 18:10:14 +0300

    Update UpdateDeploymentFile.groovy

--- jenkins/shared_library/vars/UpdateDeploymentFile.groovy
+++ jenkins/shared_library/vars/UpdateDeploymentFile.groovy
@@ -8,10 +8,8 @@ def call() {
         git pull origin main --rebase
         git stash pop || true
 
-        sed -i 's|image:.*|image: '"$ECR_REGISTRY/$ECR_REPO:$IMAGE_TAG"'|' $DEPLOYMENT_FILE_1
-        sed -i 's|image:.*|image: '"$ECR_REGISTRY/$ECR_REPO:$IMAGE_TAG"'|' $DEPLOYMENT_FILE_2
-        git add $DEPLOYMENT_FILE_1
-        git add $DEPLOYMENT_FILE_2
+        sed -i 's|image:.*|image: '"$ECR_REGISTRY/$ECR_REPO:$IMAGE_TAG"'|' $DEPLOYMENT_FILE
+        git add $DEPLOYMENT_FILE
         git commit -m "Update image to $ECR_REGISTRY/$ECR_REPO:$IMAGE_TAG" || echo "No changes to commit"
 
         git push https://$GIT_USERNAME:$GIT_PASSWORD@github.com/Orabi20/CloudDevOpsProject.git
