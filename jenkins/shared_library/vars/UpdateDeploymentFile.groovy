4525af2bc8b6605951e69becec4d30d0fd787a16
Ahmed Orabi <ahmedorabi11011@gmail.com>
2025-07-26 18:03:35 +0300

    Update UpdateDeploymentFile.groovy

--- jenkins/shared_library/vars/UpdateDeploymentFile.groovy
+++ jenkins/shared_library/vars/UpdateDeploymentFile.groovy
@@ -1,5 +1,5 @@
 def call() {
-  dir('argocd_k8s_manifest/nodejs_app_deployment') {
+  dir('argocd_k8s_manifest/argocd') {
     withCredentials([usernamePassword(credentialsId: GIT_CREDENTIALS_ID, usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_PASSWORD')]) {
       sh '''
         git config user.name "jenkins"
@@ -8,8 +8,10 @@ def call() {
         git pull origin main --rebase
         git stash pop || true
 
-        sed -i 's|image:.*|image: '"$ECR_REGISTRY/$ECR_REPO:$IMAGE_TAG"'|' $DEPLOYMENT_FILE
-        git add $DEPLOYMENT_FILE
+        sed -i 's|image:.*|image: '"$ECR_REGISTRY/$ECR_REPO:$IMAGE_TAG"'|' $DEPLOYMENT_FILE_1
+        sed -i 's|image:.*|image: '"$ECR_REGISTRY/$ECR_REPO:$IMAGE_TAG"'|' $DEPLOYMENT_FILE_2
+        git add $DEPLOYMENT_FILE_1
+        git add $DEPLOYMENT_FILE_2
         git commit -m "Update image to $ECR_REGISTRY/$ECR_REPO:$IMAGE_TAG" || echo "No changes to commit"
 
         git push https://$GIT_USERNAME:$GIT_PASSWORD@github.com/Orabi20/CloudDevOpsProject.git
