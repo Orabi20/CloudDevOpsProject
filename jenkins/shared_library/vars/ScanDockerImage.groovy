d3c8969d50d864d8970aea539967ee9f7f0739ab
orabi <rmdmedo@gmail.com>
2025-07-28 15:56:24 +0300

    update

--- jenkins/shared_library/vars/ScanDockerImage.groovy
+++ jenkins/shared_library/vars/ScanDockerImage.groovy
@@ -1,3 +1,3 @@
 def call() {
-  sh 'trivy image --format json --output trivy-report.json $ECR_REGISTRY/$ECR_REPO:$IMAGE_TAG'
+  sh 'trivy image --format json --output trivy-report.json $ECR_REPO:$IMAGE_TAG'
 }
