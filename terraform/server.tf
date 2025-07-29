fd48a0d6f2e57946a918b6f67dba63e4b40c1e82
orabi <rmdmedo@gmail.com>
2025-07-29 13:32:58 +0300

    update

--- terraform/server.tf
+++ terraform/server.tf
@@ -27,6 +27,14 @@ resource "aws_security_group" "jenkins_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
+  ingress {
+    description = "Metrics"
+    from_port   = 9100
+    to_port     = 9100
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
