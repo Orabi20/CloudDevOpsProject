2b7f5df3dfe1abed24fc189a62e2f7b3e3092bd0
Ahmed Orabi <ahmedorabi11011@gmail.com>
2025-07-29 21:28:32 +0300

    Delete terraform directory

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "orabi-tfstate"
-    key            = "eks-cluster/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-  }
-}
